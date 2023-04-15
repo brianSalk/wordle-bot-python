3b7ef95c602965b0faab1383dd9c9429ec2dc1e6
Brian <brian.salkas@gmail.com>
2023-04-14 23:14:55 -0500

    reformat main.py with black

--- main.py
+++ main.py
@@ -6,19 +6,20 @@ import random
 import collections
 import sys
 
-def filter_by_rules(bad_letters,words,correct_indexes,present_indexes,counts):
+
+def filter_by_rules(bad_letters, words, correct_indexes, present_indexes, counts):
     valid_words = []
     for word in words:
         is_valid = True
-        for char,count in counts.items():
+        for char, count in counts.items():
             if word.count(char) < count:
                 is_valid = False
                 break
         for bad in bad_letters:
             if bad in word:
                 is_valid = False
-                break 
-        for letter,indexes in correct_indexes.items():
+                break
+        for letter, indexes in correct_indexes.items():
             for index in indexes:
                 if word[index] != letter:
                     is_valid = False
@@ -28,68 +29,80 @@ def filter_by_rules(bad_letters,words,correct_indexes,present_indexes,counts):
                     is_valid = False
         if is_valid:
             valid_words.append(word)
-    
+
     return valid_words
+
+
 def get_absent_letters():
     absent_letters = set()
-    buttons = driver.find_elements(By.XPATH, "//button[contains(@class, 'Key-module_key__kchQI')]")    
+    buttons = driver.find_elements(
+        By.XPATH, "//button[contains(@class, 'Key-module_key__kchQI')]"
+    )
     for button in buttons:
-        data_state = button.get_attribute('data-state')
-        letter = button.get_attribute('data-key').upper()
-        if data_state and data_state == 'absent':
+        data_state = button.get_attribute("data-state")
+        letter = button.get_attribute("data-key").upper()
+        if data_state and data_state == "absent":
             absent_letters.add(letter)
     return absent_letters
+
+
 def get_correct_and_present_indexes():
     tile_count = 0
     correct_indexes = collections.defaultdict(list)
     present_indexes = collections.defaultdict(list)
     last_row = []
     next_row = []
-    for tile in driver.find_elements(By.XPATH, "//div[contains(@class, 'Tile-module_tile__UWEHN')]"):
-        data_state = tile.get_attribute('data-state')
+    for tile in driver.find_elements(
+        By.XPATH, "//div[contains(@class, 'Tile-module_tile__UWEHN')]"
+    ):
+        data_state = tile.get_attribute("data-state")
         letter = tile.text.upper()
-        next_row.append((letter,data_state))
-        if data_state == 'correct':
+        next_row.append((letter, data_state))
+        if data_state == "correct":
             correct_indexes[letter].append(tile_count % 5)
-        if data_state == 'present':
+        if data_state == "present":
             present_indexes[letter].append(tile_count % 5)
-        if tile_count != 0 and tile_count % 5 == 0 and next_row[0][1] != 'empty':
+        if tile_count != 0 and tile_count % 5 == 0 and next_row[0][1] != "empty":
             last_row = next_row
-            next_row = [(letter,data_state)]
+            next_row = [(letter, data_state)]
         tile_count += 1
-    counts = collections.Counter([each[0] for each in last_row if each[1] == 'present' or each[1] == 'correct'] )
+    counts = collections.Counter(
+        [each[0] for each in last_row if each[1] == "present" or each[1] == "correct"]
+    )
     return (correct_indexes, present_indexes, counts)
+
 
 def get_next_word():
     # scrape tiles
     if not absent_letters and not correct_indexes and not present_indexes:
         # return starting word
-        return ['SLATE\n', 'CRANE\n', 'SALET\n'][random.randint(0,2)]
+        return ["SLATE\n", "CRANE\n", "SALET\n"][random.randint(0, 2)]
     else:
-        next_word = words[random.randint(0,len(words)-1)]
+        next_word = words[random.randint(0, len(words) - 1)]
         return next_word
 
-if __name__ == '__main__':
-    browser = input('which browser do you use? (firefox,chrome,edge) ')
-    if browser.lower() == 'firefox':
+
+if __name__ == "__main__":
+    browser = input("which browser do you use? (firefox,chrome,edge) ")
+    if browser.lower() == "firefox":
         driver = webdriver.Firefox()
-    elif browser.lower() == 'chrome':
+    elif browser.lower() == "chrome":
         driver = webdriver.Chrome()
-    elif browser == 'edge':
+    elif browser == "edge":
         driver = webdriver.Edge()
     else:
-        print(f'{browser} not supported by this app')
+        print(f"{browser} not supported by this app")
         sys.exit(1)
 
     # load all 5-letter words from dictionary file
-    with open('five_upper') as f:
+    with open("five_upper") as f:
         words = f.readlines()
     # open url
     driver.get("https://www.nytimes.com/games/wordle/index.html")
     sleep(2.5)
-    # get keys to press 
+    # get keys to press
     keys = driver.find_elements(By.XPATH, '//button[@class="Key-module_key__kchQI"]')
-    c = driver.find_element(By.TAG_NAME, 'path')
+    c = driver.find_element(By.TAG_NAME, "path")
     sleep(1)
     c.click()
     # get enter key, press after each word
@@ -97,14 +110,16 @@ if __name__ == '__main__':
     # get backspace buton
     backspace_key = driver.find_element(By.XPATH, "//button[@aria-label='backspace']")
     while True:
-        (correct_indexes, present_indexes,counts) = get_correct_and_present_indexes()
+        (correct_indexes, present_indexes, counts) = get_correct_and_present_indexes()
         absent_letters = get_absent_letters()
-        words = filter_by_rules(absent_letters,words,correct_indexes, present_indexes,counts)
+        words = filter_by_rules(
+            absent_letters, words, correct_indexes, present_indexes, counts
+        )
         for next_letter in get_next_word():
             for key in keys:
                 if key.text == next_letter:
                     key.click()
-                if next_letter == '\n':
+                if next_letter == "\n":
                     enter_key.click()
                     for _ in range(5):
                         backspace_key.click()
