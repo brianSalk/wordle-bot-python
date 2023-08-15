34394be0560dbd3fb0c967099b8877fba074920e
BrianSalk <brian.salkas@gmail.com>
2023-08-14 23:09:11 -0500

    reformat with black

--- main.py
+++ main.py
@@ -9,12 +9,14 @@ import collections
 import sys
 
 
-def filter_by_rules(bad_letters, words, correct_indexes, present_indexes, absent_indexes, counts):
+def filter_by_rules(
+    bad_letters, words, correct_indexes, present_indexes, absent_indexes, counts
+):
     """
-   takes in bad_letters (letters not in answer), words (all words), 
-   correct_indexes ( dict associating letters to index of correct), present_indexes (dict associating letters to index of correct),
-   counts (dict counting known occurances of letter in answer)
-   returns a list of valid words according to the above rules
+    takes in bad_letters (letters not in answer), words (all words),
+    correct_indexes ( dict associating letters to index of correct), present_indexes (dict associating letters to index of correct),
+    counts (dict counting known occurances of letter in answer)
+    returns a list of valid words according to the above rules
     """
     valid_words = []
     for word in words:
@@ -72,7 +74,11 @@ def get_correct_present_and_absent_indexes():
     absent_indexes = collections.defaultdict(list)
     last_row = []
     next_row = []
-    all_tiles = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, "//div[contains(@class, 'Tile-module_tile__UWEHN')]")))
+    all_tiles = WebDriverWait(driver, 10).until(
+        EC.presence_of_all_elements_located(
+            (By.XPATH, "//div[contains(@class, 'Tile-module_tile__UWEHN')]")
+        )
+    )
     for tile in all_tiles:
         data_state = tile.get_attribute("data-state")
         letter = tile.text.upper()
@@ -122,22 +128,64 @@ if __name__ == "__main__":
         with open("five_upper") as f:
             words = f.readlines()
         # open url
-        driver.get("https://www.nytimes.com/games/wordle/index.html")
+        if len(sys.argv) == 1:
+            driver.get("https://www.nytimes.com/games/wordle/index.html")
+            purr_blocker_btn = driver.find_element(
+                By.CLASS_NAME, "purr-blocker-card__button"
+            ).click()
+            play_button = WebDriverWait(driver, 10).until(
+                EC.visibility_of_element_located(
+                    (
+                        (
+                            By.XPATH,
+                            '//button[@class="Welcome-module_button__ZG0Zh" and text()="Play"]',
+                        )
+                    )
+                )
+            )
+            play_button.click()
+            c = driver.find_element(By.TAG_NAME, "path")
+            c.click()
+        else:
+            driver.get("https://wordleunlimited.org/")
+            print("opened")
+            try:
+                overlay = WebDriverWait(driver, 5).until(
+                    EC.visibility_of_element_located((By.CLASS_NAME, "overlay"))
+                )
+                print("found overlay")
+            except Exception as e:
+                print(e)
+            driver.execute_script("arguments[0].style.visibility='hidden'", overlay)
+            print("set invis")
+            driver.find_element(By.TAG_NAME, "path").click()
+
         # click continue button, I swear they are trying to make it harder to scrape or someting
-        purr_blocker_btn = driver.find_element(By.CLASS_NAME,"purr-blocker-card__button").click()
-        play_button = WebDriverWait(driver, 10).until(EC.visibility_of_element_located(((By.XPATH, '//button[@class="Welcome-module_button__ZG0Zh" and text()="Play"]')))) 
-        play_button.click()
-        c = driver.find_element(By.TAG_NAME, "path")
-        c.click()
-        keys = driver.find_elements(By.XPATH, '//button[@class="Key-module_key__kchQI"]')
+        keys = driver.find_elements(
+            By.XPATH, '//button[@class="Key-module_key__kchQI"]'
+        )
         # get enter key, press after each word
         enter_key = driver.find_element(By.XPATH, "//button[text()='enter']")
         # get backspace buton
-        backspace_key = driver.find_element(By.XPATH, "//button[@aria-label='backspace']")
+        backspace_key = driver.find_element(
+            By.XPATH, "//button[@aria-label='backspace']"
+        )
         while True:
-            (correct_indexes, present_indexes, absent_indexes, counts) = get_correct_present_and_absent_indexes()
+            (
+                correct_indexes,
+                present_indexes,
+                absent_indexes,
+                counts,
+            ) = get_correct_present_and_absent_indexes()
             absent_letters = get_absent_letters()
-            words = filter_by_rules(absent_letters, words, correct_indexes, present_indexes, absent_indexes, counts)
+            words = filter_by_rules(
+                absent_letters,
+                words,
+                correct_indexes,
+                present_indexes,
+                absent_indexes,
+                counts,
+            )
             if not words:
                 break
 
@@ -147,16 +195,16 @@ if __name__ == "__main__":
                 for key in keys:
                     if key.text == next_letter:
                         key.click()
-                   
+
                     if next_letter == "\n":
                         enter_key.click()
                         for _ in range(5):
                             backspace_key.click()
                         sleep(1.4)
                         break
-        print('press ctrl-c to close browser')
+        print("press ctrl-c to close browser")
         while True:
             pass
     except Exception:
-        print('Looks like something went wrong, try re-running the script.')
+        print("Looks like something went wrong, try re-running the script.")
         driver.close()
